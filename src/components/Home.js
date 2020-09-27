808078f367c4755f5173e067d9c2b067e5aa8a22
Ana Pantelemon <anamaria_pante@yahoo.com>
2020-09-27 21:13:31 +0300

    added navbar and bootstap for styleing

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+//import {mester} from 'assets/images/mester.jpg';
 
 
 export default function Home(){
@@ -6,7 +7,8 @@ export default function Home(){
 
     return(
         <>
-        <h2>Home page</h2>
-             </>
+            <h2>Home page</h2>
+  
+        </>
     )
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -9,25 +9,33 @@ export default function Login(){
         const { values, bindInput } = useForm(null);
   
     function handleSubmit(event){
-        console.log(values);
-        event.preventDefault();
-        auth().signInWithEmailAndPassword(values.email,values.password);
+         event.preventDefault();
+        auth().signInWithEmailAndPassword(values.email,values.password)
+        .then(alert('Succesfully loged in'))
+        .catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+           alert(errorMessage);
+          });;
     }
 
 return(
     <>
-    <h1>Login</h1>
-    <form onSubmit={handleSubmit}>
-        <p>
-        <label htmlFor="email">Email: </label>
-        <input type='email' name="email" id="email" {...bindInput('email')} required/>
+    <div className="login">
+      <form onSubmit={handleSubmit}>
+        <p className="form-group">
+        <label htmlFor="email" >Email: </label>
+        <input type='email' name="email" id="email" {...bindInput('email')} className="form-control" aria-describedby="emailHelp" placeholder="Enter email" required/>
+        <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
         </p>
-        <p>
+        <p className="form-group">
         <label htmlFor="password">Password: </label>
-        <input type='password' name="password" {...bindInput('password')} required/>
+        <input type='password' name="password" id="password" {...bindInput('password')} className="form-control" placeholder="Password" required/>
         </p>
-        <button type="submit"> Login </button>
+        <button type="submit" className="btn btn-primary"> Submit</button>
     </form>
+    </div>
     </>
 )
 
