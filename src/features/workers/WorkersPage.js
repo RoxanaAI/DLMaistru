91c1df7bb7a3a85b05907c366c4a012a00ce191c
Roxana Ailoaie <r.ailoaie@stabiplan.ro>
2020-10-17 22:51:37 +0300

    added data in the dropw down

--- src/features/workers/WorkersPage.js
+++ src/features/workers/WorkersPage.js
@@ -1,13 +1,33 @@
-import React from 'react'
+
+import React, { useEffect, useState } from 'react'
 import WorkersList from './WorkersList';
 import WorkersFilters from './WorkersFilters';
+import * as firebase from 'firebase/app';
 import 'firebase/storage';
 
 export default function WorkersPage() {
+    const [workers, setItem] = useState([]);
+    const db = firebase.firestore();
+
+    useEffect(() => {
+        db.collection("workersCollection")
+            .onSnapshot((docs) => {
+                const workers = [];
+                docs.forEach((doc) => {
+                    const worker = {...doc.data(), id: doc.workerid}
+                    workers.push(worker);
+                });
+                setItem(workers);
+        });    
+    }, [db]);
+    
+    if(!workers) {
+        return <h1>Nu au fost adaugati maistri ...</h1>;
+    }
 
     return (
         <>
-            <WorkersFilters workers={[]}></WorkersFilters>
+            <WorkersFilters workers={workers}></WorkersFilters>
             <WorkersList firstItems={false}></WorkersList>
         </>
     );
