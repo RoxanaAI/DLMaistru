fdd143c7ea427a3f40a35dab9d19363cd29b9925
Roxana Ailoaie <r.ailoaie@stabiplan.ro>
2020-10-18 22:27:17 +0300

    added dropdown filter

--- src/features/workers/DropdownFilter.js
+++ src/features/workers/DropdownFilter.js
@@ -1,57 +1,26 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 
 import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
 
-class DropdownFilter extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          selectValue: ""
-        };
-    
-        this.handleDropdownChange = this.handleDropdownChange.bind(this);
-      }
+export const DropdownFilter = ({ dropdownList, dropDownTitle, dropDownPlaceholder }) => {
+  const [currentSelection, setCurrentSelection] = useState(dropdownList)
+  
+  const changeSelection = (newSelection) => {
+    setCurrentSelection(newSelection)
+  }
 
-      handleDropdownChange(e) {
-        this.setState({ selectValue: e.target.value });
-      }
-
-      render() {
-        return (
-            <>
-                <div className="">
-                    <label className=""> Filtru specializare </label>
-                    <Dropdown id="dropdown" options={this.list} onChange={this.handleDropdownChange} placeholder={this.placeholder} />
-                </div>
-                <div>Selected value is : {this.state.selectValue}</div>
-            </>
-        );
-      }
-
-    // state = {
-    //     selection: ""
-    // };
-
-    // handleChange = () => {
-    //     this.setState({
-    //         selection: this.state.selection
-    //     });
-    // }
-
-    // list = ["a", "b", "c", "d"];
-    // placeholder="Selecteaza";
-
-    // render() {
-    //     return (
-    //         <>
-    //             <div className="">
-    //                 <label className=""> Filtru specializare </label>
-    //                 <Dropdown options={this.list} onChange={this.handleChange} placeholder={this.placeholder} />
-    //             </div>
-    //         </>
-    //     );
-    // }
+  return (
+                <>
+                    <div className="">
+                        <label className=""> {dropDownTitle} </label>
+                        <Dropdown options = {dropdownList}
+                                  onChange = {(event) => changeSelection(event.value)}
+                                  value = {currentSelection}
+                                  placeholder = {dropDownPlaceholder} />
+                    </div>
+                </>
+            );
 }
 
 export default DropdownFilter;
--- src/features/workers/WorkersPage.js
+++ src/features/workers/WorkersPage.js
@@ -1,7 +1,7 @@
-
 import React, { useEffect, useState } from 'react'
 import WorkersList from './WorkersList';
 import WorkersFilters from './WorkersFilters';
+import DropdownFilter  from './DropdownFilter';
 import * as firebase from 'firebase/app';
 import 'firebase/storage';
 
@@ -25,8 +25,13 @@ export default function WorkersPage() {
         return <h1>Nu au fost adaugati maistri ...</h1>;
     }
 
+    let list = ["a2", "b3", "c3", "d3"];
+    let pl = "Selecteaza44";
+    let title = "titlu";
+
     return (
         <>
+            <DropdownFilter dropdownList={list} dropDownTitle={title} dropDownPlaceholder={pl}></DropdownFilter>
             <WorkersFilters workers={workers}></WorkersFilters>
             <WorkersList firstItems={false}></WorkersList>
         </>
