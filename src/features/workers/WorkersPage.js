b2bc0b64951c5281a81611a92a74f65e6dbefa6e
Roxana Ailoaie <r.ailoaie@stabiplan.ro>
2020-10-23 23:11:01 +0300

    display only the first 6 items

--- src/features/workers/WorkersPage.js
+++ src/features/workers/WorkersPage.js
@@ -5,16 +5,13 @@ import * as firebase from 'firebase/app';
 import 'firebase/storage';
 
 export default function WorkersPage( {firstItems} ) {
-    const [workers, setItem] = useState([]);
-    const [filterdWorkers, setFilteredItem] = useState(null);
-    const db = firebase.firestore();
+   const [workers, setItem] = useState([]);
+   const [primaryExists, setPrimaryExists] = useState(true);
    const [locatization, setLocalization] = useState('');
    const [specialization, setSpecialization] = useState('');
-   const [primaryExists, setPrimaryExists] = useState(true);
 
-   let clearSelection= false;
-
-    useEffect( () => {
+   const db = firebase.firestore();
+   useEffect( () => {
         async function fetchData(){
          await db.collection("workersCollection")
             .onSnapshot((docs) => {
@@ -25,8 +22,6 @@ export default function WorkersPage( {firstItems} ) {
                 });
              const sortedWorkers=sortingWorkers(workers);
              setItem(sortedWorkers);
-              console.log("in useEffect ")
-              console.log(workers);
         }); 
     }   
     fetchData();
@@ -35,44 +30,43 @@ export default function WorkersPage( {firstItems} ) {
     if(!workers) {
         return <h1>Nu au fost adaugati maistri ...</h1>;
     }
-
-
-    const dropDownData = getDropDownData(workers);
-
-    const getFilterLocalization = (selection) =>{
-        setLocalization(selection);
-        const fiteredWorkers =  workers.filter(x => x.location === selection);
-        setItem(fiteredWorkers);
-    }
-
-    const getFilterSpecialization= (selection) =>{
-        setSpecialization(selection);
-        const fiteredWorkers =  workers.filter(x => x.specialization === selection);
-        setItem(fiteredWorkers);
-    }
-
-    function resetFilter(){
-        setPrimaryExists(x=> x=!x); 
-    }
   
-        if (firstItems) {
-            const displayWorkers = workers.slice(0,3);
-            return (
-                <>
-                    <WorkersList workers={displayWorkers}></WorkersList>
-                 </>
-            );
-        } else {
-            return (
-                <>
-                    <DropdownFilter dropdownList={[...new Set(dropDownData[0])]} dropDownTitle={"Filtru localitate"} parentCallback={selection => getFilterLocalization(selection)} clearSelection={primaryExists}></DropdownFilter>
-                    <DropdownFilter dropdownList={[...new Set(dropDownData[1])]} dropDownTitle={"Filtru specializare"} parentCallback={selection => getFilterSpecialization(selection)} clearSelection={primaryExists}></DropdownFilter>
-                    <button className="btn btn-primary" onClick={resetFilter}>Resetare filtre</button>
-                    <WorkersList workers={workers}></WorkersList>
-                 </>
-            );
+    if (firstItems) {
+        const firstItemsCount = 6;
+        const displayWorkers = workers.length < firstItemsCount ? workers : workers.slice(0,firstItemsCount);
+        return (
+            <>
+                <WorkersList workers={displayWorkers}></WorkersList>
+                </>
+        );
+    } else {
+        function resetFilter(){
+            setPrimaryExists(x=> x=!x); 
         }
+
+        const getFilterLocalization = (selection) =>{
+            setLocalization(selection);
+            const fiteredWorkers =  workers.filter(x => x.location === selection);
+            setItem(fiteredWorkers);
+        }
+
+        const getFilterSpecialization= (selection) =>{
+            setSpecialization(selection);
+            const fiteredWorkers =  workers.filter(x => x.specialization === selection);
+            setItem(fiteredWorkers);
+        }
+
+        const dropDownData = getDropDownData(workers);
+        return (
+            <>
+               <DropdownFilter dropdownList={[...new Set(dropDownData[0])]} dropDownTitle={"Filtru localitate"} parentCallback={selection => getFilterLocalization(selection)} clearSelection={primaryExists}></DropdownFilter>
+               <DropdownFilter dropdownList={[...new Set(dropDownData[1])]} dropDownTitle={"Filtru specializare"} parentCallback={selection => getFilterSpecialization(selection)} clearSelection={primaryExists}></DropdownFilter>
+               <button className="btn btn-primary" onClick={resetFilter}>Resetare filtre</button>
+               <WorkersList workers={workers}></WorkersList>
+            </>
+        );
     }
+}
 
 function sortingWorkers(workers){
     const sortedWorkers= workers.slice().sort((a,b) => new Date(a.date) - new Date(b.date));
